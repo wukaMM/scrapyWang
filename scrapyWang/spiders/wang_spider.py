c77dbe67e870bdc43ab595b044f04dae65f89dc7
王宇佳 <wangyujia@cloudist.cc>
2015-05-21 11:48:54 +0800

    爬取 selenium

--- scrapyWang/spiders/wang_spider.py
+++ scrapyWang/spiders/wang_spider.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
+import os 
 
 from scrapy.spider import Spider
 from scrapy.http import Request
@@ -11,23 +12,29 @@ from soup import BeautifulSoup as bs
 class ScrapywangSpider(Spider):
     name = "wang"
     allowed_domain = []
-    start_urls = ['https://pywinauto.googlecode.com/hg/pywinauto/docs/controls_overview.html']
+    start_urls = ['http://selenium.googlecode.com/svn/trunk/docs/api/py/index.html']
 
 
     def parse(self, response):
         text_html = response.body
         soup = bs(text_html)
-        xs = soup.findAll('a', {"class" : "reference external"})
-        items = [x.get('href') for x in xs]
-        items = filter(lambda x : x, items)
+        li = soup.findAll('li', {"class" : "toctree-l3"})
+        children = [l.findChildren() for l in li]
+
+        if children:
+            items = [x[0].get('href') for x in children]
+            items = filter(lambda x : x, items)
+        else:
+            items = []
 
         for item in items:
             item = str(item)
-            url = "https://pywinauto.googlecode.com/hg/pywinauto/docs/" + item
+            url = "http://selenium.googlecode.com/svn/trunk/docs/api/py/" + item
             try:
                 r = Request(url, callback=self.parse, dont_filter=True)
                 new_item = item.replace("#", ".")
-                r.title = new_item[5:]
+                r.title = os.path.basename(new_item)
+                r.foldname = os.path.dirname(new_item)
                 text_html = text_html.replace(item, new_item)
                 yield r
             except Exception, e:
@@ -37,9 +44,10 @@ class ScrapywangSpider(Spider):
         sw['desc'] = text_html
         try:
             sw['title'] = response.request.title
+            sw['foldname'] = response.request.foldname
         except:
             sw['title'] = "index.html"
-        
+            sw['foldname'] = ""
         yield sw
 
         
